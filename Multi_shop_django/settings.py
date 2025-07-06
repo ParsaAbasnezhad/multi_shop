9edea8493d7ec9ed79653de4d2d2dd8e0f79787a
ParsaAbasnezhad <parsaabasy8@gmail.com>
2025-07-06 00:01:19 -0700

    banel model  UserManager

--- Multi_shop_django/settings.py
+++ Multi_shop_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # my_app
     'home.apps.HomeConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, 'statics')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "account.User"
