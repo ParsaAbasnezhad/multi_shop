486fea7728dec5f1569edaf12d8ce2905398511e
ParsaAbasnezhad <parsaabasy8@gmail.com>
2025-07-16 17:46:44 -0700

    Fix bog in cart

--- cart/cart_save.py
+++ cart/cart_save.py
@@ -1,26 +1,23 @@
-from enum import unique
+from shop.models import Detail
 
-from django.contrib.sites import requests
-
-CART_SESSION_ID = 'caet'
+CART_SESSION_ID = 'cart'
 
 
 class Cart:
     def __init__(self, request):
-        self.session = requests.session
+        self.session = request.session
         cart = self.session.get(CART_SESSION_ID)
         if not cart:
-            cart = self.session['cart'] = {}
+            cart = self.session[CART_SESSION_ID] = {}
         self.cart = cart
 
     def __iter__(self):
         cart = self.cart.copy()
         for item in cart.values():
-            detail = detail.objects.get(id=item['id'])
-            item['detail'] = detail
-            item['total'] = int(item['quantity']) * int(item['detail'])
-            item['unique_id'] = self.unique_id_generaton(detail.id, item['color'], item['size'])
-
+            detail_obj = Detail.objects.get(id=item['id'])
+            item['detail'] = detail_obj
+            item['total'] = int(item['quantity']) * float(detail_obj.price)
+            item['unique_id'] = self.unique_id_generaton(detail_obj.id, item['color'], item['size'])
             yield item
 
     def unique_id_generaton(self, id, color, size):
@@ -29,10 +26,17 @@ class Cart:
 
     def add(self, detail, quantity, size, color):
         unique = self.unique_id_generaton(detail.id, color, size)
-        if unique in self.cart:
-            self.cart[unique] = {'quantity': 0, 'price': str(detail.price), 'size': size, 'color': color, id: detail.id}
-
-        self.cart['quantity'][unique] += int(quantity)
+        if unique not in self.cart:
+            self.cart[unique] = {
+                'id': detail.id,
+                'quantity': 0,
+                'price': str(detail.price),
+                'size': size,
+                'color': color
+            }
+        self.cart[unique]['quantity'] += int(quantity)
+        self.save()
 
     def save(self):
+        self.session[CART_SESSION_ID] = self.cart
         self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -2,17 +2,24 @@ from cart.cart_save import Cart
 from shop.models import Detail
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
+from django.contrib import messages
 
 
 class CartDetailView(View):
     def get(self, request):
-        return render(request, 'cart/cart.html', {})
+        cart = Cart(request)
+        return render(request, 'cart/cart.html', {'cart': cart})
 
 
 class CartAddView(View):
     def post(self, request, pk):
         detail = get_object_or_404(Detail, id=pk)
-        size, color, quantity = request.POST.get('size'), request.POST.get('color'), request.POST.get('quantity')
+        size = request.POST.get('size')
+        color = request.POST.get('color')
+        quantity = request.POST.get('quantity')
+        if not size or not color or not quantity:
+            messages.error(request, 'لطفاً همه گزینه‌ها را انتخاب کنید.')
+            return redirect('shop:detail', pk=pk)
         cart = Cart(request)
-        cart.add(detail,size, color, quantity)
+        cart.add(detail, quantity, size, color)
         return redirect('cart:cart_detail')
