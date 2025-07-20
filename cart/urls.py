92dc44b2f76dd2b31e5412b708cffddfbfb9665e
ParsaAbasnezhad <parsaabasy8@gmail.com>
2025-07-19 23:47:28 -0700

    Create checkout view and logic

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,7 @@ app_name = 'cart'
 urlpatterns = [
     path('detail', views.CartDetailView.as_view(), name='cart_detail'),
     path('add/<int:pk>', views.CartAddView.as_view(), name='cart_list'),
+    path('detail/<str:id>', views.OrderDetailView.as_view(), name='cart_detail'),
+    path('order/<int:id>', views.OrderAddView.as_view(), name='cart_add'),
     path('apply/<int:pk>', views.ApplyDiscountView.as_view(), name='apply'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
 from cart.cart_save import Cart
-from cart.models import Order, DiscountCode
+from cart.models import Order, DiscountCode, OrderItem
 from shop.models import Detail
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
@@ -26,16 +26,31 @@ class CartAddView(View):
         return redirect('cart:cart_detail')
 
 
+class OrderDetailView(View):
+    def get(self, request, pk):
+        order = get_object_or_404(Order, id=pk)
+        return render(request, 'cart/order_detail.html', {'order': order})
+
+
+class OrderAddView(View):
+    def get(self, request):
+        cart = Cart(request)
+        order = Order.object.create(user=request.user, total_price=cart.total)
+        for item in cart:
+            OrderItem.objects.create(order=order, product=item['product'], price=item['price'], color=item['color'],
+                                     size=item['size'], quantity=item['quantity'])
+        return redirect('cart:order_detail', order.id)
+
+
 class ApplyDiscountView(View):
     def post(self, request, pk):
         code = request.POST.get('code')
         order = get_object_or_404(Order, id=pk)
         discount_code = get_object_or_404(DiscountCode, name=code)
         if discount_code.quantity == 0:
-            return redirect('cart:cart_detail',order.id)
+            return redirect('cart:cart_detail', order.id)
         order.total_price -= discount_code.quantity * order.price
         order.save()
         discount_code.quantity -= 1
         discount_code.save()
-        return redirect('cart:cart_detail',order.id)
-
+        return redirect('cart:cart_detail', order.id)
