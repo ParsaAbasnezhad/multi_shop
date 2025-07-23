caa4f0e1185e70a0b799506c6c622b3dec4ddbfd
ParsaAbasnezhad <parsaabasy8@gmail.com>
2025-07-22 23:43:08 -0700

    Add categories and products to the home page

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ class Category(models.Model):
     parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='subs',blank=True, null=True)
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='category/', null=True, blank=True)
 
     def __str__(self):
         return self.title
--- home/views.py
+++ home/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from home.models import Category
+from shop.models import Detail
 
 
 class HomePageView(TemplateView):
     template_name = 'home/index.html'
 
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context['product']= Detail.objects.all()
+        return context
 
 
 class CategoryListView(TemplateView):
-    template_name = 'base.html'
+    template_name = 'home/index.html'
 
     def get_context_data(self, **kwargs):
         context = super(CategoryListView, self).get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         return context
+
+
+
